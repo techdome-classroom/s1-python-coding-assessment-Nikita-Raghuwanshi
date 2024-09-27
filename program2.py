c11092197b894d3244cf30398d8f24a0aa8cf78f
Nikita Raghuwanshi <145473460+Nikita-Raghuwanshi@users.noreply.github.com>
2024-09-27 13:05:24 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -22,7 +22,7 @@ def decode_message( s: str, p: str) -> bool:
   if j < len(p) and p[j] == '*':
         return True
   return i == len(s) and j == len(p)
-print(decode_message("aa", "a*"))
+print(decode_message("aa" , "a*"))
 print(decode_message("ab","a?"))  
 print(decode_message("ab","a*"))  
 print(decode_message("abc","a*c"))    
