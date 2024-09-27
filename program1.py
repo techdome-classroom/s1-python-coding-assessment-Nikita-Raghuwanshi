45336bb7fed92082cbc43bacc8d5bd137dfd616b
Nikita Raghuwanshi <145473460+Nikita-Raghuwanshi@users.noreply.github.com>
2024-09-27 12:18:22 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -6,5 +6,6 @@ class Solution:
        def dfs(grid,i,j,visited):
         if i<0 or i>=len(grid) or j<0 or j>=len(grid[0]) or grid[i][j]=='W' or visited[i][j]:
            return 
+        
                     
         return 0
