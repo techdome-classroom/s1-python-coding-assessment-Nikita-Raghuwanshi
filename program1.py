0481ba4e51f7551a7bed37b968963e2cc57fcb8b
Nikita Raghuwanshi <145473460+Nikita-Raghuwanshi@users.noreply.github.com>
2024-09-27 12:29:18 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -6,7 +6,7 @@ class Solution:
         def dfs(grid,i,j,visited):
           if i<0 or i>=len(grid) or j<0 or j>=len(grid[0]) or grid[i][j]=='W' or visited[i][j]:
             return 
-        visited[i][j]=True
+          visited[i][j]=True
         #visited all the connected land
 
         dfs(grid,i-1,j,visited)
