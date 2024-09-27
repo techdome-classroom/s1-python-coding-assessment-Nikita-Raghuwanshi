f01a97c846a07a2a0edc111e46fcef1356387529
Nikita Raghuwanshi <145473460+Nikita-Raghuwanshi@users.noreply.github.com>
2024-09-27 12:24:16 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -19,7 +19,7 @@ class Solution:
     
         rows,cols=len(grid),len(grid[0])
        
-        visited=[[False for_in range(cols)]for]
+        visited=[[False for_in range(cols)]for_in]
 
                     
         return 0
