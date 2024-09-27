dbed2f7e69b5a3a2f2350e6c6b829b06afe1cf4d
Nikita Raghuwanshi <145473460+Nikita-Raghuwanshi@users.noreply.github.com>
2024-09-27 12:28:49 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -3,7 +3,7 @@ class Solution:
     def getTotalIsles(self, grid: list[list[str]]) -> int:
     #    write your code here
     #function to perform dfs
-       def dfs(grid,i,j,visited):
+        def dfs(grid,i,j,visited):
         if i<0 or i>=len(grid) or j<0 or j>=len(grid[0]) or grid[i][j]=='W' or visited[i][j]:
            return 
         visited[i][j]=True
