869c39d36839d45aed3068ab73d1e017a7bceb2a
Nikita Raghuwanshi <145473460+Nikita-Raghuwanshi@users.noreply.github.com>
2024-09-27 12:16:40 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -4,6 +4,6 @@ class Solution:
     #    write your code here
     #function to perform dfs
        def dfs(grid,i,j,visited):
-        if i<0 or i>=len(grid) or j<0 or j>=len
+        if i<0 or i>=len(grid) or j<0 or j>=len(grid)
                     
         return 0
