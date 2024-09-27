b322cb985a7c56996a00b064e47fadfae97cd9d8
Nikita Raghuwanshi <145473460+Nikita-Raghuwanshi@users.noreply.github.com>
2024-09-27 13:02:08 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -1,6 +1,6 @@
 def decode_message( s: str, p: str) -> bool:
 
-# write your code here
+ # write your code here
   def match_star(i,j):
         while i<len(s):
             if decode_message(s[i:],p[j+1:]):
